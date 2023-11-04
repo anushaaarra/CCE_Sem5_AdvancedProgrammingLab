9d73d3b6d10dee22149769bdd50d8a982b03565b
Anusha A <95670666+anushaaarra@users.noreply.github.com>
2023-11-04 10:33:28 +0530

    Add files via upload

---
+++ L4_Q1.py
@@ -0,0 +1,10 @@
+import math
+
+def calc(x):
+    print("Sin value of the number:", math.sin(x))
+    print("Square root of the number:", math.sqrt(x))
+    print("Log value of the number:", math.log(x))
+
+n = int(input("Enter the number: "))
+calc(n)
+    
---
+++ L4_Q5.py
@@ -0,0 +1,4 @@
+import string
+
+sen = input("Enter the sentence: ")
+print(sen.title())
